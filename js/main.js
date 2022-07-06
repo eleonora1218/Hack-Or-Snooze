7539c20812b5d49b8bbcc473f1e5da14f8da0bc7
Eleonora Bondareva <eleonora@Eleonoras-MacBook-Air.local>
2022-07-05 17:36:45 -0700

    completed Hack Or Snooze

---
+++ js/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const $body = $("body");
+const $storiesLoadingMsg = $("#stories-loading-msg");
+const $allStoriesList = $("#all-stories-list");
+const $favoritedStories = $("#favorited-stories");
+const $ownStories = $("#my-stories");
+const $storiesLists = $(".stories-list");
+const $loginForm = $("#login-form");
+const $signupForm = $("#signup-form");
+const $submitForm = $("#submit-form");
+const $navSubmitStory = $("#nav-submit-story");
+const $navLogin = $("#nav-login");
+const $navUserProfile = $("#nav-user-profile");
+const $navLogOut = $("#nav-logout");
+const $userProfile = $("#user-profile");
+const $hackOrSnoozeLink = $('#nav-all');
+const $myStoriesLink = $('#nav-my-stories');
+const $myFaveLinks = $('#nav-favorites');
+
+//To make it easier for individual components to show just themselves, this is a useful function that hides pretty much everything on the page. After calling this, individual components can re-show just what they want.
+
+function hidePageComponents() {
+  const components = [
+    $allStoriesList,
+    $loginForm,
+    $signupForm,
+  ];
+  components.forEach(c => c.hide());
+}
+
+//Overall function to kick off the app.
+async function start() {
+  console.debug("start");
+
+  //"Remember logged-in user" and log in, if credentials in localStorage
+  await checkForRememberedUser();
+  await getAndShowStoriesOnStart();
+
+  //if we got a logged-in user
+  if (currentUser) updateUIOnUserLogin();
+}
+$(start);
